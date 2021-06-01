33357068adbeb7bff7e74ef78ba48d1af5fbf94f
Prathamesh-Crypto <76525678+Prathamesh-Crypto@users.noreply.github.com>
2021-06-01 20:57:39 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+
+    fill("white")
+    text("O T I N N U F C",700,150);
+    fill("lightblue")
+    text("Hint: Performs A Task !!", 700,170);
+
+    fill("white")
+    text("E A A A S B T D",100,250);
+    fill("lightblue")
+    text("Hint: Used to store Info !!", 100,270);
+
+}
